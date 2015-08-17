673fa8bbbbec74728786432f82d6d7accd1875af
WakeDown <wakedown.59@gmail.com>
2015-08-17 18:11:04 +0500

    1

--- Code/ApiDataProvider/Helpers/AdHelper.cs
+++ Code/ApiDataProvider/Helpers/AdHelper.cs
@@ -53,11 +53,11 @@ namespace DataProvider.Helpers
 
         public static NetworkCredential GetAdUserCredentials()
         {
-            string accUserName = "UN1T\rehov";
-            string accUserPass = "R3xQwi!!";
+            string accUserName = @"UN1T\adUnit_prog";
+            string accUserPass = "1qazXSW@";
 
             string domain = "UN1T";//accUserName.Substring(0, accUserName.IndexOf("\\"));
-            string name = "rehov";//accUserName.Substring(accUserName.IndexOf("\\") + 1);
+            string name = "adUnit_prog";//accUserName.Substring(accUserName.IndexOf("\\") + 1);
 
             NetworkCredential nc = new NetworkCredential(name, accUserPass, domain);
 
