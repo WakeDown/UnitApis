01150d48151fb84a19819af4148df5207308ce7e
WakeDown <wakedown.59@gmail.com>
2015-09-18 18:03:56 +0500

    add device id to service mobile api

--- Code/ApiDataProvider/Models/Service/DeviceInfoResult.cs
+++ Code/ApiDataProvider/Models/Service/DeviceInfoResult.cs
@@ -8,6 +8,7 @@ namespace DataProvider.Models.Service
     [Serializable]
     public class DeviceInfoResult
     {
+        public int? DeviceId { get; set; }
         public string DeviceSerialNum { get; set; }
         public string ContractorStr { get; set; }
         public string ContractStr { get; set; }
